9c6a4a774f0609e449080e550d87579ae0426980
Raikonif <raikonif@gmail.com>
2023-03-28 21:23:05 -0400

    added env variables development and productions added

--- src/service/firebase.service.ts
+++ src/service/firebase.service.ts
@@ -7,16 +7,14 @@ import { API_KEY } from "~/service/service.constants";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: API_KEY,
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  // The value of `databaseURL` depends on the location of the database
-  databaseURL: "https://DATABASE_NAME.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
-  measurementId: "G-MEASUREMENT_ID",
+  apiKey: "AIzaSyBmLVui1-Mx_l1g_5JZmn2WYFaUp-wS6AQ",
+  authDomain: "pandyapk-31408.firebaseapp.com",
+  databaseURL: "https://pandyapk-31408.firebaseio.com",
+  projectId: "pandyapk-31408",
+  storageBucket: "pandyapk-31408.appspot.com",
+  messagingSenderId: "1055530752122",
+  appId: "1:1055530752122:web:8a666aa61889ec926883c3",
+  measurementId: "G-EC9CPQRDYK",
 };
 
 const app = initializeApp(firebaseConfig);
